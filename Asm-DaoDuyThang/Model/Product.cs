50e977faaf828bbef44ddbd2ee54a806ae5347e9
ThangDao01 <72856471+ThangDao01@users.noreply.github.com>
2021-10-18 13:56:24 +0700

    Add project files.

---
+++ Asm-DaoDuyThang/Model/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asm_DaoDuyThang.Model
+{
+    class Product
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AvatarPath { get; set; }
+    }
+    class Icons
+    {
+        public string AvatarPath { get; set; }
+    }
+}
